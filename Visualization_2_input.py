9e01c6223a5ff6a43325ab538f4a29f810f35e58
RikSchutte <38906316+RikSchutte@users.noreply.github.com>
2018-07-05 13:17:50 +0200

    Update Visualization_2_input.py

--- Visualization_2_input.py
+++ Visualization_2_input.py
@@ -1,15 +1,18 @@
-from bokeh.models import ColumnDataSource, DataRange1d
-from bokeh.models.glyphs import Quad
+from bokeh.models import ColumnDataSource, DataRange1d, HoverTool, TapTool
 from bokeh.io import curdoc, show
 from bokeh.plotting import figure
 
-source = ColumnDataSource()
 xdr = DataRange1d(start=5, end=15)
 ydr = DataRange1d(start=5, end=15)
 plot = figure(title=None, x_range=xdr, y_range=ydr, plot_width=320, plot_height=300,
               h_symmetry=False, v_symmetry=False, min_border=0,
-              tools="pan,wheel_zoom,box_zoom,reset,save,lasso_select,box_select,hover")
+              tools="pan,wheel_zoom,box_zoom,reset,save")
 
+plot.axis.visible = False
+plot.grid.visible = False
+
+plot.add_tools(HoverTool(tooltips=[('Name', '@names'), ('No. of leaves', '@leaves')]))
+plot.add_tools(TapTool())
 
 class Node:
     def __init__(self, level, children, leaves_subtree, color):
@@ -43,8 +46,10 @@ def foamtree(node):
         open_space_left = node.left
         open_space_top = node.top
         open_space_bottom = node.bottom
-        open_space_right = node.righ
+        open_space_right = node.right
+        rgb = (255/ maxDepth)
         for n in node.children:
+          n.color = ((255, rgb * n.level, 0))
             if not n.seen:
                 n.area = (n.leaves_subtree / n.parent.leaves_subtree) * n.parent.area
                 if (open_space_right - open_space_left) >= (open_space_top - open_space_bottom):
@@ -59,9 +64,12 @@ def foamtree(node):
                     n.right = open_space_right
                     n.bottom = -(n.area / (n.right - n.left)) + n.top
                     on_width = False
-                glyph = Quad(left=n.left, right=n.right, top=n.top,
-                             bottom=n.bottom, fill_color=n.color, fill_alpha=0.5)
-                plot.add_glyph(source, glyph)
+                source = ColumnDataSource(data=dict(names=[root.name], leaves=[root.leaves_subtree]))
+                plot.quad(left=n.left, right=n.right, top=n.top,
+                             bottom=n.bottom, fill_color=n.color, fill_alpha=0.5,
+                             hover_color=n.color, hover_alpha=1.0, name=n.name, source=source,
+                             selection_color=n.color, selection_alpha=1.0, selection_line_color='#000000',
+                             selection_line_alpha=1.0)
                 if on_width:
                     open_space_left = n.right
                 else:
@@ -76,9 +84,12 @@ def foamtree_root(root):
     root.left = 6
     root.right = 14
     root.area = (root.top - root.bottom) * (root.right - root.left)
-    glyph = Quad(left=root.left, right=root.right, top=root.top,
-                 bottom=root.bottom, fill_color=root.color, fill_alpha=0.5)
-    plot.add_glyph(source, glyph)
+    source = ColumnDataSource(data=dict(names=[root.name], leaves=[root.leaves_subtree]))
+    plot.quad(left=root.left, right=root.right, top=root.top,
+              bottom=root.bottom, fill_color=root.color, fill_alpha=0.5,
+              hover_color=root.color, hover_alpha=1.0, name=root.name, source=source,
+              selection_color=root.color, selection_alpha=1.0, selection_line_color='#000000',
+              selection_line_alpha=1.0)
     foamtree(root)
 
 
